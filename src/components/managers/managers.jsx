74bcca6056f5f10faa48a921a4fbebc06bdf0b63
Jordan Williams <jordangabrielwilliams@gmail.com>
2020-12-15 11:38:21 -0800

    Pass the year down as a prop

--- src/components/managers/managers.jsx
+++ src/components/managers/managers.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { map } from 'lodash';
 
-import DLVT from '../../data/dlvt';
+import DLVT, { all, supportedYears } from '../../data/dlvt';
 
 import Card from '../card/card';
 
 import styles from './managers.module.css';
 
-function Managers() {
+const propTypes = {
+	year: PropTypes.oneOf([all, ...supportedYears]).isRequired,
+};
+
+function Managers({ year } = {}) {
 	const managers = map(DLVT, (manager) => (
-		<Card manager={manager} key={manager.fullName} year="2020" />
+		<Card manager={manager} key={manager.fullName} year={year} />
 	));
 
-	return <div className={styles.wrapper}>{managers}</div>;
+	return <div className={styles.managers_wrapper}>{managers}</div>;
 }
+
+Managers.propTypes = propTypes;
 
 export default Managers;
